5ec598293f3f7f1cf7309449ba9e9ecb41a59723
Rique <henriquenunesti@gmail.com>
2018-06-27 01:21:04 -0300

    Tomcat 9

---
+++ src/br/com/ifpe/vox4t/controller/SistemaController.java
@@ -0,0 +1,19 @@
+package br.com.ifpe.vox4t.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+	@Author: rique
+*/
+
+@Controller
+public class SistemaController {
+	
+	@RequestMapping("home")
+	public String index() {
+		
+		return "index";
+	}
+}
+
