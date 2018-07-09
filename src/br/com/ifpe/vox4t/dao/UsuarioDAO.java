d7142cc25d083c675b1b1115915d1d4627de3619
Henrique Nunes Mendes <henriquenunesti@gmail.com>
2018-07-08 23:29:11 -0300

    Login funcionando.

--- src/br/com/ifpe/vox4t/dao/UsuarioDAO.java
+++ src/br/com/ifpe/vox4t/dao/UsuarioDAO.java
@@ -30,40 +30,44 @@ public class UsuarioDAO {
 		factory.close();
 	}
 
-	public boolean logar(Usuario usuario) {
+public boolean logar(String emailUsuario, String senhaUsuario) {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
 		EntityManager manager = factory.createEntityManager();
 		Query query = null;
-		query = manager.createQuery("FROM Usuario");
-		/*
-		WHERE email = :paramEmail AND senha = :paramSenha"
-		query.setParameter("paramEmail", email);
-		query.setParameter("paramSenha", senha);
-		*/
-		System.out.println(usuario.getEmail());
+		query = manager.createQuery("FROM Usuario WHERE email = :paramEmail AND senha = :paramSenha");
+		query.setParameter("paramEmail", emailUsuario);
+		query.setParameter("paramSenha", senhaUsuario);
+		@SuppressWarnings("unchecked")
 		List<Usuario> lista = query.getResultList();
 		manager.close();
 		factory.close();
 		
 		if (lista.size() > 0) {
+			System.out.println("Encontrou");
 			return true;
 		} else {
+			System.out.println("Não encontrou");
 			return false;
 		}
 	
 	}
-/*
-	 
-	public Usuario logar(String email,String password){
-		DetachedCriteria detachedCriteria =  DetachedCriteria.forClass(Usuario.class);
-		detachedCriteria.add(Restrictions.eq("email", email));
-		detachedCriteria.add(Restrictions.eq("senha", password));
-		List<Usuario> findByCriteria = (List<Usuario>) hibernateTemplate.findByCriteria(detachedCriteria);
-		if(findByCriteria !=null && findByCriteria.size()>0)
-		return findByCriteria.get(0);
-		else
-			return null;
-}
-*/
+	public Usuario buscarPorEmail(String email) {
+
+		Usuario obj = null;
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+		EntityManager manager = factory.createEntityManager();
+		Query query = null;
+		query = manager.createQuery("FROM Usuario WHERE email = :paramEmail");
+		query.setParameter("paramEmail", email);
+		
+		obj = (Usuario) query.getSingleResult();
+		
+		manager.close();
+		factory.close();
+		
+		return obj;
+	    }
+
 
 }
