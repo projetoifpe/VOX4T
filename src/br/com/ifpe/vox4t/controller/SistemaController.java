5a03d2ee752eb31d3f2b85f5ebab7e4b95140eb5
marly <marly@DESKTOP-LD6AUH7>
2018-07-12 17:09:20 -0300

    edição de alguns arquivos jsp e teste de twitter

--- src/br/com/ifpe/vox4t/controller/SistemaController.java
+++ src/br/com/ifpe/vox4t/controller/SistemaController.java
@@ -3,20 +3,33 @@ package br.com.ifpe.vox4t.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+import org.springframework.ui.Model;
+import java.util.List;
+import br.com.ifpe.vox4t.dao.UsuarioDAO;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+/*
 /**
  * @Author: rique.
  */
 
 @Controller
 public class SistemaController {
-
+	
 	@RequestMapping("home")
 	public String index() {
 		return "index";
 	}
-
-	/*@RequestMapping("/teste")
-	public String teste() {
-		return "teste";
-	}*/
+	/*
+	@RequestMapping("testeTwitter")
+	public String testeTwitter(Model model) throws TwitterException {
+		UsuarioDAO user = new UsuarioDAO();
+		List<Status> listaTweets = user.testeTwitter();
+		
+		model.addAttribute("listaTweets", listaTweets);
+		return "testeTT";
+	}
+*/
+	
 }
