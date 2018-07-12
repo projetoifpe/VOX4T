c8cd768b61389f69a741ec836236ac43d639b1d8
ALUNO <ALUNO@cgti-ThinkCentre-E73z>
2018-07-12 18:32:42 -0500

    INCLUSÃO TWITTER4J

--- src/br/com/ifpe/vox4t/dao/UsuarioDAO.java
+++ src/br/com/ifpe/vox4t/dao/UsuarioDAO.java
@@ -9,11 +9,11 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import br.com.ifpe.vox4t.model.Usuario;
-/*import twitter4j.Status;
+import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
-*/
+
 /**
  * @Author: henrique
  */
@@ -76,7 +76,7 @@ public class UsuarioDAO {
 		return obj;
 	    
 	}
-	/*
+	//ESTE METODO VAI PRA CLASSE TWITTER FUTURAMENTE
 	public List<Status> testeTwitter() throws TwitterException {
 		
 		ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -85,14 +85,16 @@ public class UsuarioDAO {
 					.setOAuthConsumerKey("Wc1xqD1ZXDFD1M2xoPJSwU687")
 					.setOAuthConsumerSecret("REiwyUuT8RkI580qxQ9L6qz4XsI6WSLssR1UtjnuX4uMEe8KF8")
 					.setOAuthAccessToken("987757693040545797-wlQva0FLSy6ZQPcrqt6KxiFNnArhms1")
-					.setOAuthAccessTokenSecret("55j6SRHL31KKFC8CC4HH2QdISyMZJs9q8hsch9zF41Fuz");
-
+					.setOAuthAccessTokenSecret("55j6SRHL31KKFC8CC4HH2QdISyMZJs9q8hsch9zF41Fuz")
+					.setTweetModeExtended(true);
+				
 		TwitterFactory tf = new TwitterFactory(cb.build());
 		twitter4j.Twitter twitter = tf.getInstance();
+		
 		
 		List<Status> status = twitter.getUserTimeline("@jctransito");
 		
 		return status;
 	}
-*/
+
 }
