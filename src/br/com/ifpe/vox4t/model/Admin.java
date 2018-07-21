7288208ddeda77cae6c5be320ea9474618645dc1
projetoifpe <38770941+projetoifpe@users.noreply.github.com>
2018-07-21 16:09:13 -0300

    Admin

---
+++ src/br/com/ifpe/vox4t/model/Admin.java
@@ -0,0 +1,55 @@
+package br.com.ifpe.vox4t.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="administrador")
+public class Admin {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_administrador")
+	private int id;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String senha;
+
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	
+}
